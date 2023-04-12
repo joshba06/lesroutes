29eabc223ad5f41336722500bca1b2203335bf19
Niklas Bakula <niklasjbakula@gmail.com>
2023-04-12 15:34:31 +0200

    Implemented dropdown (dots - multilevel) on index page. Links work, sharing feature not yet

--- app/javascript/controllers/route_controller.js
+++ app/javascript/controllers/route_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="route"
 export default class extends Controller {
-  static targets = ["time", "distance", "routeTitle", "formUpdateRouteTitle", "filterMenu"]
+  static targets = ["time", "distance", "routeTitle", "formUpdateRouteTitle", "filterMenu", "dropdownElement"]
   static values = {
     time: Number,
     distance: Number,
@@ -39,5 +39,9 @@ export default class extends Controller {
 
   }
 
+  toggle_dropdown (event) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
 
 }
