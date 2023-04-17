88a7a17c5022dbf43b358c77418074a0bdc117e6
Niklas Bakula <niklasjbakula@gmail.com>
2023-04-17 18:29:48 +0200

    Fixed layout for signup and signin page on mobile. Removed destination images from seeds file. Added 'name' column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_154625) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_162000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_154625) do
     t.string "last_name"
     t.string "age"
     t.string "location"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
