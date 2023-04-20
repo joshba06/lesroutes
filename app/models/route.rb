1f9d483e593c3c840388eedce605cbc01887e14b
Niklas Bakula <niklasjbakula@gmail.com>
2023-04-20 17:56:59 +0200

    Added model validation and alerts to prevent user from creating more than 10 routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,8 +1,20 @@
 class Route < ApplicationRecord
+  validate :user_has_too_many_routes
+
   belongs_to :user
   has_many :route_destinations, dependent: :destroy
   has_many :destinations, through: :route_destinations
   has_one_attached :photo
+
   validates :city, presence: true
   validates :title, presence: true
+
+
+  private
+
+  def user_has_too_many_routes
+    if Route.where(user_id: self.user_id).length >= 10
+      errors.add(:too_many, "too many routes")
+    end
+  end
 end
