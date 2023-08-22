f28c8eb9fdb40478b995d686d0f7de2d9e87022a
Niklas Bakula <niklasjbakula@gmail.com>
2023-08-22 13:19:58 +0200

    Corrected google directions url generation for sharing via email, sms and whatsapp

--- app/javascript/controllers/sharing_controller.js
+++ app/javascript/controllers/sharing_controller.js
@@ -9,23 +9,26 @@ export default class extends Controller {
 
   connect() {
     this.subject = "LesRoutes - Route Link"
-    this.message = `Hey mate, check out this route:\n\n${this.routeUrlValue}\n\nI found it on a free website called lesroutes.co.uk that lets you create and manage routes for Google Maps.`
+    const link = this.routeUrlValue.replace(/\s/g, "%20").replace(/\|/g, "%7C").replace(/,/, "%2C")
+    this.body = `Hey mate, check out this route:\n\n${link}\n\nI found it on a free website called lesroutes.co.uk that lets you create and manage routes for Google Maps.`
+    console.log(this.body)
   }
 
   send_email () {
-    window.location.href = `mailto:"Mate"?subject=${this.subject}&body=${encodeURIComponent(this.message)}`;
+    window.location.href = "mailto:?subject=" + encodeURIComponent(this.subject) + "&body=" + encodeURIComponent(this.body);
+
   }
 
   send_whatsapp () {
-    window.location.href = `https://wa.me/?text=${encodeURIComponent(this.message)}`;
+    window.location.href = `https://wa.me/?text=${encodeURIComponent(this.body)}`;
   }
 
   send_sms () {
     if(navigator.userAgent.match(/Android/i)) {
-      window.open(`sms://1234/?body=${encodeURIComponent(this.message)}`)
+      window.open(`sms://1234/?body=${encodeURIComponent(this.body)}`)
     }
     else if(navigator.userAgent.match(/iPhone/i)) {
-      window.open(`sms://1234/&body=${encodeURIComponent(this.message)}`)
+      window.open(`sms://1234/&body=${encodeURIComponent(this.body)}`)
      }
   }
 }
